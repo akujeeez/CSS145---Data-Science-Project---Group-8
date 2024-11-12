ecdbff351e36fec427a1137c185104d49f9b2225
ash <143350076+a1freed@users.noreply.github.com>
2024-11-13 02:37:03 +0800

    Update css145_proposal_2project_group_8.py

--- css145_proposal_2project_group_8.py
+++ css145_proposal_2project_group_8.py
@@ -51,9 +51,9 @@ import pandas as pd
 # Load the challenger match data for initial analysis
 
 
-challenger_df = pd.read_csv('challenger_match.csv')
-match_winner_data = pd.read_csv('match_winner_data_version1.csv')
-match_losser_data = pd.read_csv('match_loser_data_version1.csv')
+challenger_df = pd.read_csv('challenger_match.csv', delimiter=';')
+match_winner_data = pd.read_csv('match_winner_data_version1.csv', delimiter=';')
+match_losser_data = pd.read_csv('match_loser_data_version1.csv', delimiter=';')
 
 st.write(match_winner_data.head())
 st.write(match_losser_data.head())
